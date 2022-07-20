90bba2119cade9281ceb2e6984fca209b77ecbb9
heidegomes <heidegc@hotmail.com>
2022-07-20 19:18:54 -0300

    exercicio das aulas assincronas

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-primeiros-passos/exercicio1/scripts.js
@@ -0,0 +1,15 @@
+const myName = "Heide";
+const birthCity = "São Paulo";
+let birthYear = 1986;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "São Bernardo do Campo";
+
+console.log(birthCity);
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-primeiros-passos/exercicio5/script.js
@@ -0,0 +1,7 @@
+let weekDay = "sabado";
+
+if (weekDay = "segunda-feira" || "terça-feira" ||"quarta-feira" || "quinta-feira" || "sexta-feira") {
+    console.log ("Oba, mais um dia de aprendizado na Trybe >:D");
+} else if (weekDay = "sabado" || "domingo") {
+    console.log ("FINALMENTE, descanso merecido UwU");
+}
