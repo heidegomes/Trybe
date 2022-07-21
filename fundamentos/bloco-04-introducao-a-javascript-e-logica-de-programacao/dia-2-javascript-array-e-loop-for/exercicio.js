be931d42044a1e63a2aafd2092a88bfc5efdd70a
heidegomes <heidegc@hotmail.com>
2022-07-21 19:25:48 -0300

    adicionando pasta de exercicios do dia 2 no bloco 4.

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -0,0 +1,103 @@
+ //Exercício 1
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index < numbers.length; index++) {
+//     console.log (numbers[index]);
+// }
+
+  //Exercício 2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index++) {
+//     soma += numbers[index];
+// }
+
+// console.log (soma);
+
+//Exercicio 3
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index++) {
+//     soma += numbers[index];
+// }
+
+// console.log (soma);
+
+// let media = soma / numbers.length;
+
+// console.log (media);
+
+// Exerício 4
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index++) {
+//     soma += numbers[index];
+// }
+
+// let media = soma / numbers.length;
+// console.log (soma);
+// console.log (media);
+
+// if (media > 20) {
+//     console.log('Valor maior que 20!');
+// } else {
+//     console.log('Valor menor ou igual a 20');
+// }
+
+//Exercicio 5 - ****** Não resolvido ******
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let maiorValor = 0;
+
+// for (let index = 0; index < numbers.length; index++) {
+//     if ( maiorValor[index] > numbers[index] ) {
+//         maiorValor += index;
+//     }
+// }
+
+// console.log (maiorValor);
+
+// Exercicio 6 - ****** Não resolvido ******
+// let numbers = [5, 9, 3, -19, 70, 8, 100, 2, 35, 27];
+// let impar = 0;
+
+// for (let index = 0; index < numbers.length; index++) {
+//     if (numbers[index] % 2 != 0) {
+//         impar += numbers[index];
+//     }
+// }
+
+// console.log (impar);
+
+// Exercicio 7 - ****** Não resolvido ******
+// let numbers = [5, 9, 3, -19, 70, 8, 100, 2, 35, 27];
+// let impar = 0;
+
+// for (let index = 0; index < numbers.length; index++) {
+//     if (numbers[index] % 2 != 0) {
+//         impar += numbers[index];
+//     }
+// }
+
+// console.log (par);
+
+//Exercicio 8 - ****** Não resolvido ******
+
+// let array = [];
+// for (let index = 0; index < 26; index++) {
+//     array.push();
+// }
+
+// console.log (array)
+
+//Exercicio 9 - ****** Não resolvido ******
+
+// let array = [];
+// for (let index = 0; index < 26; index++) {
+//     array.push();
+// }
+
+// console.log (array)
