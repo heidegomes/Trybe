5a4eb428cc7054999f4c9ef19aac0151a3970ae4
heidegomes <heidegc@hotmail.com>
2022-07-26 09:09:04 -0300

    adicionando exercicios das aulas assincronas

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/study.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/study.js
@@ -1,55 +1,55 @@
-// let diasDaSemana = {
-//   1: 'domingo',
-//   2: 'segunda',
-//   3: 'terça',
-//   4: 'quarta',
-//   5: 'quinta',
-//   6: 'sexta',
-//   7: 'sábado',
-//   };
+let diasDaSemana = {
+  1: 'domingo',
+  2: 'segunda',
+  3: 'terça',
+  4: 'quarta',
+  5: 'quinta',
+  6: 'sexta',
+  7: 'sábado',
+  };
   
-//   diasDaSemana.1; // SyntaxError: Unexpected number
-//   console.log(diasDaSemana['1']); // domingo
+  diasDaSemana.1; // SyntaxError: Unexpected number
+  console.log(diasDaSemana['1']); // domingo
 
-// let conta = {
-//   agencia: '0000',
-//   banco: {
-//     cod: '012',
-//     id: 4,
-//     nome: 'TrybeBank',
-//   },
-// };
+let conta = {
+  agencia: '0000',
+  banco: {
+    cod: '012',
+    id: 4,
+    nome: 'TrybeBank',
+  },
+};
 
-// let infoDoBanco = 'banco';
-// console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
-// console.log(conta[infoDoBanco]['nome']); // TrybeBank
+let infoDoBanco = 'banco';
+console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+console.log(conta[infoDoBanco]['nome']); // TrybeBank
 
-// console.log(conta.agencia); // 0000
-// console.log(conta['agencia']); // 0000
+console.log(conta.agencia); // 0000
+console.log(conta['agencia']); // 0000
 
-// console.log(conta.banco.cod); // 012
-// console.log(conta['banco']['id']); // 4
+console.log(conta.banco.cod); // 012
+console.log(conta['banco']['id']); // 4
 
-// let usuario = {
-//   id: 99,
-//   email: 'jakeperalta@gmail.com',
-//   infoPessoal: {
-//     nome: 'Jake',
-//     sobrenome: 'Peralta',
-//     endereco: {
-//       rua: 'Smith Street',
-//       bairro: 'Brooklyn',
-//       cidade: 'Nova Iorque',
-//       estado: 'Nova Iorque',
-//     },
-//   },
-// };
+let usuario = {
+  id: 99,
+  email: 'jakeperalta@gmail.com',
+  infoPessoal: {
+    nome: 'Jake',
+    sobrenome: 'Peralta',
+    endereco: {
+      rua: 'Smith Street',
+      bairro: 'Brooklyn',
+      cidade: 'Nova Iorque',
+      estado: 'Nova Iorque',
+    },
+  },
+};
 
-// console.log(usuario['id']); // 99
-// console.log(usuario.email); // jakeperalta@gmail.com
+console.log(usuario['id']); // 99
+console.log(usuario.email); // jakeperalta@gmail.com
 
-// console.log(usuario.infoPessoal.endereco.rua); // Smith Street
-// console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
+console.log(usuario.infoPessoal.endereco.rua); // Smith Street
+console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
 
 let moradores = [
   {
