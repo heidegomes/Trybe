92b4e765e3d3cf3ece95865997cec7ddd0722fb2
heidegomes <heidegc@hotmail.com>
2022-07-26 09:13:51 -0300

    adicionando exercicios do para fixar.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/paraFixar.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/paraFixar.js
@@ -1,34 +1,34 @@
 /*1*/
 
-// let player = {
-//     infoPessoal: {
-//         name: 'Marta',
-//         lastName: 'Silva',
-//         age: 34,  
-//     },
-//     medals: { 
-//         golden: 2, 
-//         silver: 3, 
-//     },
-//     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
-// }
-//     console.log ( 'A ' + player.infoPessoal.name + ' ' + player.infoPessoal.lastName + ' tem ' + player.infoPessoal.age + ' anos de idade.');
+let player = {
+    infoPessoal: {
+        name: 'Marta',
+        lastName: 'Silva',
+        age: 34,  
+    },
+    medals: { 
+        golden: 2, 
+        silver: 3, 
+    },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+}
+    console.log ( 'A ' + player.infoPessoal.name + ' ' + player.infoPessoal.lastName + ' tem ' + player.infoPessoal.age + ' anos de idade.');
 
-//     console.log ('A ' + player.infoPessoal.name + ' ' + player.infoPessoal.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld + ' vezes.');
+    console.log ('A ' + player.infoPessoal.name + ' ' + player.infoPessoal.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
 
-//     console.log ('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
+    console.log ('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
 
     /*For in **** For of*/
     /*1*/
-// let names = {
-//     person1: 'João',
-//     person2: 'Maria',
-//     person3: 'Jorge',
-// };
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+};
 
-// for (let index in names) {
-//     console.log('Olá ' + names[index]); //imprime os valores
-// };
+for (let index in names) {
+    console.log('Olá ' + names[index]); //imprime os valores
+};
 
     /*2*/
 let car = {
